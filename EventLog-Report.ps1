c4e07ce2958278382c80f0b3724609ed25c94d0e
linal3650 <47990405+linal3650@users.noreply.github.com>
2020-04-17 17:36:09 -0400

    Create EventLog-Report.ps1

---
+++ EventLog-Report.ps1
@@ -0,0 +1,25 @@
+# Get a breakdown of error sources in the System Event Log
+
+Param(
+    [string]$Log = "System",
+    [string]$Computername = $env:COMPUTERNAME,
+    [int32]$Newest = 500,
+    [string]$ReportTitle = "Event Log Report",
+    [Parameter(HelpMessage = "Enter the path for the HTML file.")]
+    [string]$Path
+)
+
+#get event log data and group it
+$data = Get-EventLog -LogName $Log -EntryType Error -Newest $Newest -ComputerName $Computername |
+    Group-Object -Property Source -NoElement
+
+#create an HTML report
+$footer = "<h5><i>Report run on $(Get-Date)</i></h5>"
+$precontent = "<h1>$Computername</h1><h2>Last $Newest error sources from $Log</h2>"
+
+$data | Sort-Object -Property Count,Name -Descending |
+    Select-Object Count,Name |
+    ConvertTo-Html -Title $ReportTitle -PreContent $precontent -PostContent $footer -CssUri $css |
+    Out-File -FilePath $Path
+    
+#.\paramscript.ps1 -Path .\systemresources.html
